1a0ad9f152d8db0eefd7e1383232a3bc4cf517e8
Pankaj Bhanu <bhanu.pankaj7711@gmail.com>
2025-03-29 15:41:06 +0530

    minor change

--- data/datasets/register_coco.py
+++
@@ -1,1 +0,0 @@
-from .coco import register_coco_instances  # noqa
--- modeling/arch/__init__.py
+++
@@ -1,8 +0,0 @@
-from .build import ARCH_REGISTRY, build_model  # isort:skip
-
-# import all the arch, so they will be registered
-from .dense_detector import DenseDetector
-from .retinanet import RetinaNet
-
-
-__all__ = list(globals().keys())
